815ec63243096f15fbec3b18f45719bf1d492bcc
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-12-25 12:28:46 -0300

    Add redirect to login if the user is not logged in

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async ({ locals, url }) => {
+	const loginPath = '/login';
+	const user = locals.user;
+
+	if (!user && url.pathname !== loginPath) {
+		return redirect(307, loginPath);
+	}
+
+	return { user };
+};
