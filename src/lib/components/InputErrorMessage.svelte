4cc6d2a630eb31346990c5ae57c81654d90af690
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-12-25 12:31:29 -0300

    Add InputErrorMessage component

---
+++ src/lib/components/InputErrorMessage.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { getInputContext } from '$lib/contexts/input-provider/context';
+
+	const input = getInputContext();
+</script>
+
+<p class="h-6 text-red-600 font-semibold">{$input.error || ''}</p>
