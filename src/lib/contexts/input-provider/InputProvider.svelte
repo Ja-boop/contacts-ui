a9cf36e8c16ca9d0fcf5ba8580554cb718dd91b4
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-12-25 12:16:25 -0300

    Add Input provider
    - Add InputContext

---
+++ src/lib/contexts/input-provider/InputProvider.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { formErrors } from '$lib/stores/form-errors';
+	import { getInputContext, setInputContext } from './context';
+
+	export let name: string;
+	export let type: string;
+
+	setInputContext();
+
+	const input = getInputContext();
+
+	$input.name = name;
+	$input.type = type;
+
+	$: if ($formErrors) {
+		$input.error = $formErrors[name]?.[0];
+	}
+</script>
+
+<slot />
