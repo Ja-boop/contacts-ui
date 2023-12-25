f4f270e731101e28bfb75fa915db691aebdc2ff3
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-12-25 12:14:33 -0300

    Add Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+	interface $$Props extends HTMLButtonAttributes {
+		className: string;
+	}
+
+	export let className = '';
+</script>
+
+<button
+	on:click
+	class={`${className} bg-[#9378FF] py-4 flex items-center justify-center text-white rounded-full hover:brightness-90 active:brightness-100`}
+	{...$$restProps}
+>
+	<slot />
+</button>
