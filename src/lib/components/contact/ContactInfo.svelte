edc98ab05fc3a504e63a35b6a348fc7205954b5f
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2024-01-01 11:29:38 -0300

    Add ContactInfo component

---
+++ src/lib/components/contact/ContactInfo.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let title: string;
+	export let text: string;
+</script>
+
+<div class="flex flex-col gap-2">
+	<h3 class="text-xl text-primary-text font-bold">{title}</h3>
+	<p class="text-base text-secondary-text font-semibold">{text}</p>
+</div>
