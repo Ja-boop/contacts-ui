107124b127998b248442e4b52a301a21f58bcac8
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2024-01-01 11:40:18 -0300

    Return home as a default backPath in layout server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,11 +3,12 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
 	const loginPath = '/login';
+	const homePath = '/';
 	const user = locals.user;
 
 	if (!user && url.pathname !== loginPath) {
 		return redirect(307, loginPath);
 	}
 
-	return { user };
+	return { user, backPath: homePath };
 };
