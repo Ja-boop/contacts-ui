5ae3aca1e988911cfb349fc69784d5aceb4ddd9d
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2024-01-01 11:35:01 -0300

    Add search input component

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,11 @@
+<script>
+	import SearchIcon from '$lib/icons/SearchIcon.svelte';
+	import Input from '$lib/components/Input.svelte';
+
+	export let value = '';
+</script>
+
+<search class="relative flex flex-row items-center">
+	<SearchIcon class="absolute ml-2" />
+	<Input class="w-full h-11 border-secondary border-2 rounded-lg pl-10 pr-5" type="search" name="search" bind:value />
+</search>
