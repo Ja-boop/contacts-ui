b6f5d002ea15c09456f37f68431687963120c68c
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2024-01-02 20:26:56 -0300

    Add upload file command for cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,7 +35,21 @@
 //     }
 //   }
 // }
+import 'cypress-file-upload';
 
 Cypress.Commands.add('getByDataTestAttribute', (dataTestAttribute, ...args) => {
 	return cy.get(`[data-cy=${dataTestAttribute}]`, ...args);
+});
+
+import 'cypress-file-upload';
+Cypress.Commands.add('upload_file', (fileName, fileType = ' ', selector) => {
+	cy.get(selector).then((subject) => {
+		cy.fixture(fileName, 'base64').then((content) => {
+			const el = subject[0] as HTMLInputElement;
+			const testFile = new File([content], fileName, { type: fileType });
+			const dataTransfer = new DataTransfer();
+			dataTransfer.items.add(testFile);
+			el.files = dataTransfer.files;
+		});
+	});
 });
